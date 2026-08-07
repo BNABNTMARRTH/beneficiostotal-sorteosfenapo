7d35a8fdbe3749cb26fb0068fe4ba6864959a787
BNABNTMARRTH <dissobente@gmail.com>
2026-08-07 14:03:33 -0600

    Update live backend tunnel URL to bald-conversations-robinson-forestry.trycloudflare.com

--- public_html/js/rifa-v1.js
+++ public_html/js/rifa-v1.js
@@ -4,7 +4,7 @@
 (function () {
     'use strict';
 
-    var API_BASE = (document.currentScript && document.currentScript.dataset.api) || 'https://hope-laboratories-supplements-reporters.trycloudflare.com';
+    var API_BASE = (document.currentScript && document.currentScript.dataset.api) || 'https://bald-conversations-robinson-forestry.trycloudflare.com';
 
     var REGISTRO_KEY = 'tp_rifa_registro';
     var VISTO_KEY = 'tp_rifa_visto';
